59cd64171ca53e2619aaa96dc15c9d8b96990ea0
aicha <aicha.draoui-etu@etu.univh2c.ma>
2023-07-08 12:35:21 +0100

    first_commit

---
+++ src/test/java/com/example/spring_prj1/SpringPrj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_prj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrj1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
